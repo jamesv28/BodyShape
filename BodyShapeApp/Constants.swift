0a147b0f713b1805a88fcacf30ad35166e70552b
James Volmert <james.volmert@gmail.com>
2025-07-30 17:30:42 -0600

    added custom font

--- BodyShapeApp/Constants.swift
+++ BodyShapeApp/Constants.swift
@@ -3,4 +3,4 @@ import SwiftUI
 
 let lightGreen = Color("LightGreen")
 let lightPurple = Color("LightPurple")
-
+//let jakartaSans = .font(.custom("PlusJakartaSans-Regular"))
---
+++ BodyShapeApp/Dashboard/IconRight.swift
@@ -0,0 +1,18 @@
+//
+//  IconRight.swift
+//  BodyShapeApp
+//
+//  Created by James Volmert on 7/30/25.
+//
+
+import SwiftUI
+
+struct IconRight: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconRight()
+}
