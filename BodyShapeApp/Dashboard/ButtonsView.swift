3d23ceb0be8afa8c978c84d9a741950ef5566e47
James Volmert <james.volmert@gmail.com>
2025-08-02 18:21:52 -0600

    button view

--- BodyShapeApp/Dashboard/ButtonsView.swift
+++ BodyShapeApp/Dashboard/ButtonsView.swift
@@ -1,15 +1,54 @@
-//
-//  ButtonsView.swift
-//  BodyShapeApp
-//
-//  Created by James Volmert on 7/31/25.
-//
-
 import SwiftUI
 
 struct ButtonsView: View {
+    let imgNames = [
+        "house.fill",
+        "bookmark.fill",
+        "alarm.fill",
+        "arrow.up.right.square.fill"
+    ]
+    let actionName = [
+        "house",
+        "bookmark",
+        "alarm",
+        "stats"
+    ]
+    
+    @State private var selectedImage = "house.fill"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ForEach(0..<imgNames.count, id: \.self) {img in
+                OneButtonView(imgName: imgNames[img], selectedImage: $selectedImage, action: {print("some action: \(img)")})
+            }
+        }
+    }
+}
+
+
+struct OneButtonView: View {
+    let imgName: String
+    @Binding var selectedImage: String
+    var action: () -> Void = {
+        print("buton pressed")
+    }
+    var isSelected: Bool {
+        selectedImage == imgName
+    }
+    var body: some View {
+        Button {
+            selectedImage = imgName
+            action()
+        } label: {
+            Image(systemName: imgName)
+                .imageScale(.large)
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.black)
+                .padding()
+                .background(isSelected ? lightGreen.opacity(0.7) : .clear)
+                .clipShape(Circle())
+        }
+
     }
 }
 
